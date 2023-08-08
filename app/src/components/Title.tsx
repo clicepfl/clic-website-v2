0a25fea699aa8f5d226a7c5020bbb1ba4fc8e4f8
Noé Terrier <terriernoe@gmail.com>
2023-08-08 16:41:27 +0200

    feat: add Title component

---
+++ app/src/components/Title.tsx
@@ -0,0 +1,11 @@
+export default function Title({ text }: { text?: string }) {
+  return (
+    <div className="flex items-center">
+      <div className="bg-blue-950 w-10 h-0.5 mr-2" />
+      <h1 className="uppercase font-mono font-bold text-blue-950">
+        {text ? text : "Default title"}
+      </h1>
+      <div className="bg-blue-950 w-10 h-0.5 ml-2" />
+    </div>
+  );
+}
