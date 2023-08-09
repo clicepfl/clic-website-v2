00b5dfef5529de06397389532aaa0064ee9ced44
Noé Terrier <noeterrier@proton.me>
2023-08-09 17:56:08 +0200

    feat: text props is now mandatory
    Co-authored-by: Ludovic Mermod <ludovic.mermod@epfl.ch>

--- app/src/components/Title.tsx
+++ app/src/components/Title.tsx
@@ -3,7 +3,7 @@ export default function Title({ text }: { text: string }) {
     <div className="flex items-center">
       <div className="bg-blue-950 w-10 h-0.5 mr-2" />
       <h1 className="uppercase font-mono font-bold text-blue-950">
-        {text ? text : "Default title"}
+        {text}
       </h1>
       <div className="bg-blue-950 w-10 h-0.5 ml-2" />
     </div>
