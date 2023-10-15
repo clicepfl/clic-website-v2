e3472df6aef86ad747cd7f5c4dc22fbc8d0cbb3b
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-10-15 02:09:52 +0200

    feat: center text in Title

--- app/src/components/Title.tsx
+++ app/src/components/Title.tsx
@@ -10,12 +10,12 @@ export default function Title({
   const linesClassNames = `w-10 h-0.5 ${dark ? "bg-blue-950" : "bg-white"}`;
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center">
       <div className={linesClassNames} />
       <h1
-        className={
-          `uppercase font-mono font-bold mx-2 ${textSize} ${dark ? "text-blue-950" : "text-white"}`
-        }
+        className={`uppercase font-mono font-bold mx-2 ${textSize} ${
+          dark ? "text-blue-950" : "text-white"
+        }`}
       >
         {text}
       </h1>
