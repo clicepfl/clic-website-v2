db221a62c5739440eea0a6493e6e6c50b7b80266
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-10-18 18:41:35 +0200

    feat: start homepage

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,16 @@
-import EventCard from "@/components/EventCard";
+import background from "assets/background.svg";
+import clicLogo from "assets/clic_logo_white.svg";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="p-10 bg-slate-200 w-screen h-screen">
-      <EventCard id={1} vertical />
+    <div
+      className="p-10 h-full flex flex-col items-center h-screen bg-cover"
+      style={{ backgroundImage: `url(${background.src})` }}
+    >
+      <div className="w-full h-[12rem] relative">
+        <Image fill className="object-fit" src={clicLogo} alt="CLIC" />
+      </div>
     </div>
   );
 }
